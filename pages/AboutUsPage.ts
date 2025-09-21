7058be3df36558e6d7c2e79086a9afe0bcca7fbd
Princess Diana Nava Remoroza <diane.nava014@gmail.com>
2025-09-21 21:20:19 +0800

    chore: added test to info.spec.ts, created AboutUsPage, integrated POM+fixtures

---
+++ pages/AboutUsPage.ts
@@ -0,0 +1,46 @@
+import { Page, Locator, expect } from '@playwright/test';
+
+export class AboutUsPage {
+
+   readonly AboutUsLink: Locator;
+   readonly HomeLink: Locator;
+   readonly ServicesLink: Locator;
+   readonly TestingAsAServiceLink: Locator;
+   readonly TestimonialsLink: Locator;
+   readonly BlogLink: Locator;
+  readonly ContactUsLink: Locator;
+  readonly PracticeNowLink: Locator;
+  // Locators
+
+    constructor(public readonly page: Page) {
+    // -> Initialize locators
+
+     this.AboutUsLink = page.getByRole('navigation').getByRole('link', { name: 'About Us' });
+     this.HomeLink = page.getByRole('navigation').getByRole('link', { name: 'Home' });
+     this.ServicesLink = page.getByRole('button', { name: 'Services ▼' });
+     this.TestingAsAServiceLink = page.getByRole('link', { name: 'Testing-As-A-Service' });
+     this.TestimonialsLink = page.getByRole('navigation').getByRole('link', { name: 'Testimonials' });
+     this.BlogLink = page.getByRole('navigation').getByRole('link', { name: 'Blog' });
+     this.ContactUsLink = page.getByRole('navigation').getByRole('link', { name: 'Contact Us' }); 
+      this.PracticeNowLink = page.getByRole('button', { name: 'Practice Now' });
+
+     }
+
+  // methods
+
+   async navigateTo(): Promise<void> {
+    await this.page.goto('https://testautomationph.netlify.app/');
+     await this.AboutUsLink.click();
+  }
+
+  async VerifyNavigationMenuItems(): Promise<void> {
+    await expect(this.HomeLink).toBeVisible();
+    await expect(this.ServicesLink).toBeVisible();
+    await expect(this.TestimonialsLink).toBeVisible();
+    await expect(this.BlogLink).toBeVisible();
+    await expect(this.AboutUsLink).toBeVisible();
+    await expect(this.ContactUsLink).toBeVisible();
+    await expect(this.PracticeNowLink).toBeVisible();
+  }
+
+}
---
+++ shared/base.ts
@@ -0,0 +1,43 @@
+import { test as base, request } from '@playwright/test';
+import { AboutUsPage } from '../pages/AboutUsPage';
+// import { RegistrationPage } from '@pages/registration.page';
+// import { APIModel } from '@pages/api.model';
+// import { FakeStoreAPI } from '@pages/api-fake-store.model';
+
+type MyFixtures = {
+  AboutUsPage: AboutUsPage;
+//   loginPage: LoginPage;
+//   registrationPage: RegistrationPage;
+//   apiJsonPlaceholder: APIModel;
+//   fakeStoreAPI: FakeStoreAPI;
+};
+
+export const test = base.extend<MyFixtures>({
+  AboutUsPage: async ({ page }, use) => {
+    await use(new AboutUsPage(page));
+  },
+//   loginPage: async ({ page }, use) => {
+//     await use(new LoginPage(page));
+//   },
+//   registrationPage: async ({ page }, use) => {
+//     await use(new RegistrationPage(page));
+//   },
+//   apiJsonPlaceholder: async ({}, use) => {
+//     const requestContext = await request.newContext({
+//       baseURL: 'https://jsonplaceholder.typicode.com',
+//     });
+//     const apiModel = new APIModel(requestContext);
+//     await use(apiModel);
+//     await requestContext.dispose();
+//   },
+//   fakeStoreAPI: async ({}, use) => {
+//     const requestContext = await request.newContext({
+//       baseURL: 'https://fakestoreapi.com',
+//     });
+//     const fakeStore = new FakeStoreAPI(requestContext);
+//     await use(fakeStore);
+//     await requestContext.dispose();
+//   },
+});
+
+export { expect } from '@playwright/test';
